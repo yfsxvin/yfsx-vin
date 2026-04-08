38cd853100ad97d748527b40d8cae46ad1792775
yfsxvin <6558971@gmail.com>
2026-04-08 11:30:50 +0800

    Create VIN Token smart contract

---
+++ contracts/bsc/VINToken.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract VINToken is ERC20, Ownable {
+    constructor(uint256 initialSupply) ERC20("VIN Token", "VIN") {
+        _mint(msg.sender, initialSupply);
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+}
